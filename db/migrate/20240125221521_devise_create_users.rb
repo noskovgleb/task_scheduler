ff882f3b964b02b7846b314a8a118abd013b4497
Gleb N <noskovgleb@gmail.com>
2024-01-26 01:32:20 +0200

    Initial commit

---
+++ db/migrate/20240125221521_devise_create_users.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :email,              null: false, default: "", index: { unique: true }
+      t.string :encrypted_password, null: false, default: ""
+      t.string   :reset_password_token, index: { unique: true }
+      t.datetime :reset_password_sent_at
+      t.timestamps null: false
+    end
+  end
+end
